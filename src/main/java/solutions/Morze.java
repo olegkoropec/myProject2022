c3b09b77dd91f7a536e86893639d2279e9e81262
Oleh Koropets <k.olegu23@gmail.com>
2022-09-16 20:50:21 +0300

    Yes

--- src/main/java/solutions/Morze.java
+++ src/main/java/solutions/Morze.java
@@ -1,24 +1,54 @@
 package solutions;
+import java.util.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class Morze {
-    public static void main(String[] args) {
-        StringBuilder build = new StringBuilder();
-        Map <Character, String> map = new HashMap<Character, String>();
-        String morze = "england";
+class Morze {
+    public int uniqueMorseRepresentations(String[] words) {
+        Map <Character, String> map = new HashMap<>();
         map.put('a', ".-");
-        map.put('e', ".");
-        map.put('g', "--.");
+        map.put('b', "-...");
+        map.put('c', "-.-.");
         map.put('d', "-..");
+        map.put('e', ".");
+        map.put('f', "..-.");
+        map.put('g', "--.");
+        map.put('h', "....");
+        map.put('i', "..");
+        map.put('j', ".---");
+        map.put('k', "-.-");
         map.put('l', ".---");
+        map.put('m', "--");
         map.put('n', "-");
+        map.put('o', "---");
+        map.put('p', ".--.");
+        map.put('q', "--.-");
+        map.put('r', ".-.");
+        map.put('s', "...");
+        map.put('t', "-");
+        map.put('u', "..-");
+        map.put('v', "...-");
+        map.put('w', ".--");
+        map.put('x', "-..-");
+        map.put('y', "-.--");
+        map.put('z', "--..");
 
-        for (int i = 0; i < morze.length(); i ++){
-            morze.charAt(i);
-           build.append(map.get(morze.charAt(i)));
-            System.out.println(" " + build);
+        Set<String> set = new HashSet<>();
+
+        for (String word : words) {
+            StringBuilder build = new StringBuilder();
+            for (int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+                build.append(map.get(ch));
+            }
+            String string = build.toString();
+            set.add(string);
+            System.out.println(string);
         }
+        System.out.println(set.size());
+        return set.size();
+    }
+    public static void main(String[] args) {
+        Morze solution = new Morze();
+        String [] words = {"rwjje","aittjje","auyyn","lqtktn","lmjwn"};
+        solution.uniqueMorseRepresentations(words);
     }
 }
